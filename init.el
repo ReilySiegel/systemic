e79c052984b46374bd4c8ac1daff97895aa24db0
Reily Siegel <mail@reilysiegel.com>
2021-08-26 14:20:32 -0400

    emacs: Use xelatex by default for org export

--- init.el
+++ init.el
@@ -320,10 +320,9 @@ Passes NAME and ARGS to use-package."
         org-plantuml-jar-path
         plantuml-jar-path
 
-        ;; org-latex-pdf-process
-        ;; '("xelatex -interaction nonstopmode %f"
-        ;;   "xelatex -interaction nonstopmode %f")
-        )
+        org-latex-pdf-process
+        '("xelatex -interaction nonstopmode %f"
+          "xelatex -interaction nonstopmode %f"))
   ;; Set (no) indentation
   (setq org-adapt-indentation nil)
   ;; Log time a task was set to Done.
