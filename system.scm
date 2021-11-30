d0c1823776d2d3b8f83d3b10d8847c7625b384d4
Reily Siegel <mail@reilysiegel.com>
2021-11-29 21:35:29 -0500

    system: Fix bluetooth
    The persistent bluetooth issues when using this configuration were caused by a
    poor interaction between GDM and PulseAudio. Replacing GDM with SLiM has fixes
    this issue.

--- system.scm
+++ system.scm
@@ -31,74 +31,83 @@
                  (home-directory "/home/reily")
                  (supplementary-groups
                   '("wheel" "netdev" "audio" "video" "input")))
-                %qbase-user-accounts))
+                %base-user-accounts))
   (packages
    (append
-    (list (specification->package "emacs")
-          (specification->package "emacs-exwm")
-          (specification->package "nss-certs")
-          (specification->package "pulseaudio")
-          (specification->package "mesa")
-          (specification->package "alsa-utils"))
-    %base-packages))
+       (list (specification->package "emacs")
+             (specification->package "emacs-exwm")
+             (specification->package "nss-certs")
+             (specification->package "pulseaudio")
+             (specification->package "mesa")
+             (specification->package "alsa-utils"))
+       %base-packages))
   (services
    (append
-    (list 
-     (service openssh-service-type)
-     (udev-rules-service 'backlight brightnessctl)
-     (service systemic-bluetooth-service-type
-              (systemic-bluetooth-configuration
-               (auto-enable? #t)))
-     (service tlp-service-type)
-     (service cups-service-type
-              (cups-configuration
-               (web-interface? #t)
-               (max-clients 1000)
-               (timeout 10)
-               (extensions
-                (list cups-filters epson-inkjet-printer-escpr hplip-minimal))))
-     (set-xorg-configuration
-      (xorg-configuration
-       (modules (delq (specification->package "xf86-input-synaptics")
-                      %default-xorg-modules))
-       (drivers '("modesetting" "intel"))
-       (keyboard-layout keyboard-layout)
-       (extra-config '("
+       (list
+        (service slim-service-type
+                 (slim-configuration
+                  (xorg-configuration
+                   (xorg-configuration
+                    (modules (delq (specification->package "xf86-input-synaptics")
+                                   %default-xorg-modules))
+                    (drivers '("modesetting" "intel"))
+                    (keyboard-layout keyboard-layout)
+                    (extra-config '("
 Section \"InputClass\"
 Identifier \"devname\"
 MatchIsTouchpad \"on\"
 Driver \"libinput\"
 Option \"NaturalScrolling\" \"true\"
-EndSection")))))
-    (modify-services %desktop-services
-      (elogind-service-type
-       config =>
-       (elogind-configuration
-        (handle-power-key 'suspend)
-        (idle-action 'suspend)
-        (idle-action-seconds (* 5 60))))
-      (upower-service-type
-       config =>
-       (upower-configuration
-        (use-percentage-for-policy? #t)
-        (percentage-low 15)
-        (percentage-critical 10)
-        (percentage-action 5)
-        (critical-power-action 'power-off)))
-      (guix-service-type
-       config =>
-       (guix-configuration
-        (inherit config)
-        (substitute-urls
-         (append %default-substitute-urls
-                 (list "https://mirror.brielmaier.net")))
-        (authorized-keys
-         (append (list (local-file "mirror.brielmaier.net.pub"))
-                 %default-authorized-guix-keys)))))))
+EndSection"))))))
+        (service openssh-service-type)
+        (udev-rules-service 'backlight brightnessctl)
+        (service bluetooth-service-type
+                 (bluetooth-configuration
+                  (auto-enable? #t)))
+        (service tlp-service-type)
+        (service cups-service-type
+                 (cups-configuration
+                  (web-interface? #t)
+                  (max-clients 1000)
+                  (timeout 10)
+                  (extensions (list cups-filters
+                                    epson-inkjet-printer-escpr
+                                    hplip-minimal)))))
+       (modify-services %desktop-services
+         (delete gdm-service-type)
+         (elogind-service-type
+          config =>
+          (elogind-configuration
+           (inherit config)
+           (handle-power-key 'suspend)
+           ;; FIXME: Laptop always reports OnExternalPower=yes
+           (handle-lid-switch-external-power 'suspend)
+           (idle-action 'suspend)
+           (idle-action-seconds (* 5 60))))
+         ;; TODO: Auto-power off on low battery
+         (tlp-service-type
+          config =>
+          (tlp-service-configuration
+           (inherit config)
+           (tlp-default-mode "BAT")
+           (cpu-scaling-governor-on-ac "performance")
+           (cpu-scaling-governor-on-bat "powersave")
+           (cpu-boost-on-ac? #t)
+           (sched-powersave-on-bat? #t)))
+         (guix-service-type
+          config =>
+          (guix-configuration
+           (inherit config)
+           (substitute-urls
+            (append %default-substitute-urls
+                (list "https://mirror.brielmaier.net")))
+           (authorized-keys
+            (append (list (local-file "mirror.brielmaier.net.pub"))
+                %default-authorized-guix-keys)))))))
   (bootloader
    (bootloader-configuration
     (bootloader grub-efi-bootloader)
-    (target "/boot/efi")
+    (targets '("/boot/efi"))
     (keyboard-layout keyboard-layout)))
   (file-systems
    (cons* (file-system
