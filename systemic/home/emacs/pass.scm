f3dcc594c9ee61d5d41e32322153a5d8ba68e3ea
Reily Siegel <mail@reilysiegel.com>
2022-01-12 21:24:21 -0500

    home: emacs: Ignore tests for password-store.

--- systemic/home/emacs/pass.scm
+++ systemic/home/emacs/pass.scm
@@ -5,15 +5,19 @@
   #:use-module (gnu packages password-utils)
   #:use-module (systemic home emacs-utils)
   #:use-module (systemic packages emacs-xyz)
+  #:use-module (systemic packages)
   #:export (pass-configuration))
 
+;; HACK: Remove this once tests for password-store are fixed.
 (define pass-configuration
-  (elisp-configuration-package
-   "pass"
-   '()
-   #:elisp-packages (list emacs-pass
-                          emacs-password-store-otp
-                          pass-otp
-                          qrencode
-                          imagemagick
-                          zbar)))
+  (without-tests
+   password-store
+   (elisp-configuration-package
+    "pass"
+    '()
+    #:elisp-packages (list emacs-pass
+                           emacs-password-store-otp
+                           pass-otp
+                           qrencode
+                           imagemagick
+                           zbar))))
