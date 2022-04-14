8c9ee40213d6722df82d4e0b4eff8b4e1877db46
Reily Siegel <mail@reilysiegel.com>
2022-04-14 14:20:25 +0200

    home: Use emacs-doom-themes instead of emacs-nord.
    * systemic/home/emacs.scm (theme-service, services): New variable.
    (theme-configuration): Removed.
    (packages): New variable.
    (emacs-packages): Removed.
    * systemic/home/luden.scm (reily-home-environment): Remove emacs service, add
    emacs:services list.

--- systemic/home/emacs.scm
+++ systemic/home/emacs.scm
@@ -1,14 +1,19 @@
 (define-module (systemic home emacs)
+  #:use-module (gnu home-services-utils)
+  #:use-module (gnu home-services emacs)
   #:use-module (gnu packages emacs-xyz)
   #:use-module (gnu packages statistics)
+  #:use-module (gnu services)
+  #:use-module (guix gexp)
   #:use-module (guix transformations)
+  #:use-module (rrr packages emacs-xyz)
   #:use-module (systemic home emacs-utils)
   #:use-module (systemic home emacs completion)
   #:use-module (systemic home emacs org)
   #:use-module (systemic home emacs pass)
+  #:use-module (systemic packages emacs)
   #:use-module (systemic packages emacs-xyz)
-  #:export (emacs-packages))
-
+  #:export (services))
 
 (define default-configuration
   (elisp-configuration-package
@@ -19,6 +24,7 @@
        (menu-bar-mode -1)
        (tool-bar-mode -1)
        (scroll-bar-mode -1)
+       (line-number-mode -1)
        (winner-mode 1)
        (show-paren-mode 1))
 
@@ -36,48 +42,54 @@
      (defalias 'yes-or-no-p 'y-or-n-p))
    #:autoloads? #t))
 
-(define theme-configuration
-  (elisp-configuration-package
-   "theme"
-   `((set-frame-font "Hack-11")
-
-     (require 'nord-theme)
-     (if (daemonp)
-         (add-hook 'after-make-frame-functions
-		   (lambda (frame)
-		     (select-frame frame)
-		     (load-theme 'nord t)))
-         (load-theme 'nord t))
-
-     (line-number-mode 0)
-
-     (require 'time)
+(define theme-service
+  (simple-service
+   'emacs-theme-service
+   home-emacs-service-type
+   (emacs-configuration-extension
+    ("font"
+     (set-frame-font "Hack-11"))
+    (emacs-doom-themes
+     (load-theme 'doom-nord t)
+     ;; Fix for issue with tab-bar-mode
+     (custom-set-faces
+      `(tab-bar
+        ((t (:background ,(doom-color 'bg-alt) :foreground ,(doom-color 'fg-alt)))))))
+    (emacs-doom-modeline
      (require 'doom-modeline)
 
-     (doom-modeline-mode 1)
-     
-     (setq display-time-24hr-format t
-           display-time-interval 5
-           display-time-default-load-average nil
-           doom-modeline-enable-word-count nil
-           doom-modeline-buffer-file-name-style 'truncate-with-project
-           doom-modeline-enable-word-count t
-           doom-modeline-percent-position nil
-           doom-modeline-buffer-encoding nil
-           doom-modeline-major-mode-icon nil
-           doom-modeline-buffer-modification-icon nil
-           doom-modeline-buffer-state-icon nil)
-     (display-time-mode 1)
-     (display-battery-mode 1))
-   #:elisp-packages (list emacs-doom-modeline
-                          emacs-nord-theme)
-   #:autoloads? #t))
+     ;; Use default battery display.
+     (remove-hook 'display-battery-mode-hook #'doom-modeline-override-battery-modeline)
+     (remove-hook 'doom-modeline-mode-hook #'doom-modeline-override-battery-modeline)
+
+     (setopt display-time-24hr-format t
+             display-time-day-and-date t
+             display-time-interval 5
+             display-time-default-load-average nil
+             battery-update-interval 5
+             battery-mode-line-format "[%p%% %b%t] "
+             doom-modeline-enable-word-count nil
+             doom-modeline-buffer-file-name-style 'truncate-with-project
+             doom-modeline-enable-word-count t
+             doom-modeline-percent-position nil
+             doom-modeline-buffer-encoding nil
+             doom-modeline-major-mode-icon nil
+             doom-modeline-buffer-modification-icon nil
+             doom-modeline-buffer-state-icon nil)
+
+     ;; Use tab-bar as global modeline.
+     (setopt tab-bar-format '(tab-bar-format-align-right tab-bar-format-global)
+             tab-bar-mode t)
+
+     (doom-modeline-mode)
+     (display-time-mode)
+     (display-battery-mode)))))
 
 (define emacs-lsp-java-transform
   (options->transformation
    '((with-branch . "emacs-lsp-java=master"))))
 
-(define emacs-packages
+(define packages
   (list
    emacs-use-package emacs-esup
    emacs-avy emacs-no-littering
@@ -94,9 +106,33 @@
    emacs-clj-refactor emacs-origami-el
    emacs-project
    default-configuration
-   theme-configuration
    org-configuration
    org-agenda-configuration
    org-roam-configuration
    candidate-configuration
    pass-configuration))
+
+(define services
+  (list
+   (service home-emacs-service-type
+            (home-emacs-configuration
+             (package emacs-next-no-pgtk)
+             (rebuild-elisp-packages? #f)
+             (elisp-packages packages)
+             (init-el
+              (list (slurp-file-gexp (local-file "../../init.el"))))
+             (early-init-el
+              '((set 'gc-cons-threshold most-positive-fixnum)
+                (run-at-time "20 sec" nil
+                             (lambda ()
+                               (set 'gc-cons-threshold 800000)))
+                
+                (setq package-enable-at-startup nil)
+                
+                (defvar my/file-name-handler-alist file-name-handler-alist)
+                (setq file-name-handler-alist nil)
+                (add-hook 'emacs-startup-hook
+                          (lambda ()
+                            (setq file-name-handler-alist
+                                  my/file-name-handler-alist)))))))
+   theme-service))
--- systemic/home/luden.scm
+++ systemic/home/luden.scm
@@ -1,13 +1,11 @@
 (define-module (systemic home luden)
   #:use-module (gnu home)
-  #:use-module (gnu home-services emacs)
   #:use-module (gnu home-services gnupg)
   #:use-module (gnu home-services mail)
   #:use-module (gnu home-services password-utils)
   #:use-module (gnu home services shells)
   #:use-module (gnu home services shepherd)
   #:use-module (gnu home-services ssh)
-  #:use-module (gnu home-services-utils)
   #:use-module (gnu home services)
   #:use-module (gnu services)
   #:use-module (gnu packages compton)
@@ -18,76 +16,56 @@
   #:use-module (srfi srfi-26)
   #:use-module (systemic home bibliography)
   #:use-module (systemic home desktop)
-  #:use-module (systemic home emacs)
+  #:use-module ((systemic home emacs) #:prefix emacs:)
   #:use-module (systemic home git)
-  #:use-module (systemic home mail)
-  #:use-module (systemic packages emacs))
+  #:use-module (systemic home mail))
 
 (define-public reily-home-environment
   (home-environment
    (services
-    (list
-     (service picom-service-type #f)
-     (service exwm-service-type)
-     (simple-service 'syncthing
-                     home-shepherd-service-type
-                     (list (shepherd-service
-                            (documentation "Run Syncthing.")
-                            (provision '(syncthing))
-                            (start #~(make-forkexec-constructor '("syncthing"
-                                                                  "-no-browser"
-                                                                  "-no-restart")))
-                            (stop #~(make-kill-destructor)))))
-     (service systemic-mail-service-type)
-     (service bibliography-service-type
-              (bibliography-configuration
-               (bibtex-file "~/org/references.bib")
-               (csl-directory "~/org/csl")))
-     (service home-gnupg-service-type
-	      (home-gnupg-configuration
-               (gpg-agent-config
-                (home-gpg-agent-configuration
-                 (ssh-agent? #t)
-                 (pinentry-flavor 'emacs)))))
-     (service systemic-git-service-type)
-     (service home-ssh-service-type
-              (home-ssh-configuration
-               (toplevel-options
-                '((match .
-                    "host * exec \"gpg-connect-agent UPDATESTARTUPTTY /bye\"")))))
-     (service home-bash-service-type
-              (home-bash-configuration))
-     (service home-emacs-service-type
-              (home-emacs-configuration
-               (package emacs-next-no-pgtk)
-               (rebuild-elisp-packages? #f)
-               (elisp-packages emacs-packages)
-               (init-el
-                (list (slurp-file-gexp (local-file "../../init.el"))))
-               (early-init-el
-                '((set 'gc-cons-threshold most-positive-fixnum)
-                  (run-at-time "20 sec" nil
-                               (lambda ()
-                                 (set 'gc-cons-threshold 800000)))
-                  
-                  (setq package-enable-at-startup nil)
-                  
-                  (defvar my/file-name-handler-alist file-name-handler-alist)
-                  (setq file-name-handler-alist nil)
-                  (add-hook 'emacs-startup-hook
-                            (lambda ()
-                              (setq file-name-handler-alist
-                                    my/file-name-handler-alist)))))))
-     (simple-service
-      'some-useful-env-vars-service
-      home-environment-variables-service-type
-      `(("GUILE_LOAD_PATH" .
-         "$XDG_CONFIG_HOME/guix/current/share/guile/site/3.0:$GUILE_LOAD_PATH")
-        ("GUILE_LOAD_COMPILED_PATH" .
-         "$XDG_CONFIG_HOME/guix/current/lib/guile/3.0/site-ccache:$GUILE_LOAD_COMPILED_PATH")
-        ("GUILE_AUTO_COMPILE" . "0")
-        ("PATH" . "$HOME/.local/bin:$PATH")
-        ("npm_config_prefix" . "$HOME/.local")
-        ("NODE_PATH" . "$HOME/.local/lib/node_modules:$NODE_PATH")
-        ;; HACK: https://issues.guix.gnu.org/52672
-        ("QTWEBENGINE_CHROMIUM_FLAGS" . "--disable-seccomp-filter-sandbox")))))))
+    (append
+     emacs:services
+     (list
+      (service picom-service-type #f)
+      (service exwm-service-type)
+      (simple-service 'syncthing
+                      home-shepherd-service-type
+                      (list (shepherd-service
+                             (documentation "Run Syncthing.")
+                             (provision '(syncthing))
+                             (start #~(make-forkexec-constructor '("syncthing"
+                                                                   "-no-browser"
+                                                                   "-no-restart")))
+                             (stop #~(make-kill-destructor)))))
+      (service systemic-mail-service-type)
+      (service bibliography-service-type
+               (bibliography-configuration
+                (bibtex-file "~/org/references.bib")
+                (csl-directory "~/org/csl")))
+      (service home-gnupg-service-type
+	       (home-gnupg-configuration
+                (gpg-agent-config
+                 (home-gpg-agent-configuration
+                  (ssh-agent? #t)
+                  (pinentry-flavor 'emacs)))))
+      (service systemic-git-service-type)
+      (service home-ssh-service-type
+               (home-ssh-configuration
+                (toplevel-options
+                 '((match .
+                     "host * exec \"gpg-connect-agent UPDATESTARTUPTTY /bye\"")))))
+      (service home-bash-service-type
+               (home-bash-configuration))
+      (simple-service
+       'some-useful-env-vars-service
+       home-environment-variables-service-type
+       `(("GUILE_LOAD_PATH" .
+          "$XDG_CONFIG_HOME/guix/current/share/guile/site/3.0:$GUILE_LOAD_PATH")
+         ("GUILE_LOAD_COMPILED_PATH" .
+          "$XDG_CONFIG_HOME/guix/current/lib/guile/3.0/site-ccache:$GUILE_LOAD_COMPILED_PATH")
+         ("GUILE_AUTO_COMPILE" . "0")
+         ("PATH" . "$HOME/.local/bin:$PATH")
+         ("npm_config_prefix" . "$HOME/.local")
+         ("NODE_PATH" . "$HOME/.local/lib/node_modules:$NODE_PATH")
+         ;; HACK: https://issues.guix.gnu.org/52672
+         ("QTWEBENGINE_CHROMIUM_FLAGS" . "--disable-seccomp-filter-sandbox"))))))))
