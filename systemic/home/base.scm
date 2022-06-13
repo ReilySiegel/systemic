f6120712443553188d22f32d2bd584d7d7852415
Reily Siegel <mail@reilysiegel.com>
2022-06-13 09:20:42 -0400

    home: Move services that require password-store out of base.
    * systemic/home/base.scm (environment): Remove syncthing, mail, and bibliography
    services.
    * systemic/home/luden.scm (environment): Add syncthing, mail, and bibliography
    services.

--- systemic/home/base.scm
+++ systemic/home/base.scm
@@ -1,18 +1,11 @@
 (define-module (systemic home base)
   #:use-module (gnu home)
-  #:use-module (gnu home-services mail)
-  #:use-module (gnu home-services password-utils)
   #:use-module (gnu home services shells)
-  #:use-module (gnu home services shepherd)
   #:use-module (gnu home services)
   #:use-module (gnu services)
-  #:use-module (guix gexp)
   #:use-module (srfi srfi-26)
-  #:use-module (systemic home bibliography)
-  #:use-module (systemic home desktop)
   #:use-module ((systemic home emacs) #:prefix emacs:)
-  #:use-module (systemic home git)
-  #:use-module (systemic home mail))
+  #:use-module (systemic home git))
 
 (define-public environment
   (home-environment
@@ -20,20 +13,6 @@
     (append
      emacs:services
      (list
-      (simple-service 'syncthing
-                      home-shepherd-service-type
-                      (list (shepherd-service
-                             (documentation "Run Syncthing.")
-                             (provision '(syncthing))
-                             (start #~(make-forkexec-constructor '("syncthing"
-                                                                   "-no-browser"
-                                                                   "-no-restart")))
-                             (stop #~(make-kill-destructor)))))
-      (service systemic-mail-service-type)
-      (service bibliography-service-type
-               (bibliography-configuration
-                (bibtex-file "~/org/references.bib")
-                (csl-directory "~/org/csl")))
       (service systemic-git-service-type)
       (service home-bash-service-type
                (home-bash-configuration))
--- systemic/home/luden.scm
+++ systemic/home/luden.scm
@@ -1,15 +1,35 @@
 (define-module (systemic home luden)
+  #:use-module (gnu home services shepherd)
   #:use-module (gnu home)
-  #:use-module (gnu services)
   #:use-module (gnu home-services gnupg)
+  #:use-module (gnu home-services mail)
+  #:use-module (gnu home-services password-utils)
   #:use-module (gnu home-services ssh)
+  #:use-module (gnu services)
+  #:use-module (guix gexp)
   #:use-module (systemic home desktop)
-  #:use-module ((systemic home base) #:prefix base:))
+  #:use-module ((systemic home base) #:prefix base:)
+  #:use-module (systemic home bibliography)
+  #:use-module (systemic home mail))
 
 (define-public environment
   (home-environment
    (services
     (cons*
+     (simple-service 'syncthing
+                     home-shepherd-service-type
+                     (list (shepherd-service
+                            (documentation "Run Syncthing.")
+                            (provision '(syncthing))
+                            (start #~(make-forkexec-constructor '("syncthing"
+                                                                  "-no-browser"
+                                                                  "-no-restart")))
+                            (stop #~(make-kill-destructor)))))
+     (service systemic-mail-service-type)
+     (service bibliography-service-type
+              (bibliography-configuration
+               (bibtex-file "~/org/references.bib")
+               (csl-directory "~/org/csl")))
      (service picom-service-type #f)
      (service exwm-service-type)
      (service home-gnupg-service-type
