e0ebaab107fe7d2475d1f6f0bfc514cfc5c2d6ca
Reily Siegel <mail@reilysiegel.com>
2022-06-13 22:48:46 -0400

    home: emacs: editing: Replace proselint with languagetool.
    * systemic/home/emacs/editing.scm (service): Use languagetool.

--- systemic/home/emacs/editing.scm
+++ systemic/home/emacs/editing.scm
@@ -3,6 +3,7 @@
   #:use-module (gnu home-services emacs)
   #:use-module (gnu services)
   #:use-module (systemic home emacs-utils)
+  #:use-module (systemic packages emacs-xyz)
   #:export (service))
 
 (define service
@@ -12,8 +13,11 @@
     ('flymake
      (add-hook 'text-mode-hook #'flymake-mode)
      (add-hook 'prog-mode-hook #'flymake-mode))
-    (emacs-flymake-proselint
-     (add-hook 'text-mode-hook #'flymake-proselint-setup))
+    (emacs-flymake-languagetool
+     (setopt
+      flymake-languagetool-server-jar
+      (substitute-in-file-name "$XDG_DATA_HOME/languagetool/languagetool-server.jar"))
+     (add-hook 'text-mode-hook #'flymake-languagetool-load))
     ('eldoc
      (setopt eldoc-documentation-strategy 'eldoc-documentation-compose))
     (emacs-avy
