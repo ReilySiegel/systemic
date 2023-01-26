145555ee54a9d135982992741f842da15e774495
Reily Siegel <mail@reilysiegel.com>
2023-01-26 13:10:02 -0500

    home: emacs: Misc changes to emacs.

--- systemic/home/emacs/shell.scm
+++ systemic/home/emacs/shell.scm
@@ -11,11 +11,14 @@
    (emacs-configuration-extension
     ("eshell"
      (keymap-global-set "C-c s" #'eshell)
+     (require 'tramp)
+     (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
      (with-eval-after-load 'eshell
        (require 'esh-module)
        (add-to-list 'eshell-modules-list 'eshell-tramp)
-       (setopt eshell-destroy-buffer-when-process-dies t
-               eshell-history-size 1024
+       (setopt esheXll-destroy-buffer-when-process-dies t
+               eshell-history-size 4096
+               eshell-hist-ignoredups t
                remote-file-name-inhibit-cache nil
                vc-ignore-dir-regexp
                (format "%s\\|%s" vc-ignore-dir-regexp tramp-file-name-regexp)
