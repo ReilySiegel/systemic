592f2c3442aa903bddb6d9c2de66f731c83f3eaf
Reily Siegel <mail@reilysiegel.com>
2023-08-08 09:40:58 -0400

    Revert "channels: Use custom Guix channel."
    This reverts commit 166eec22298bebfd8843aad717c76ef4dc9a62e9.

--- systemic/channels.scm
+++ systemic/channels.scm
@@ -15,7 +15,7 @@
        (pretty-print (cons 'list (map channel->code channels)) port)))))
 
 (define %systemic-channels
-  (list
+  (cons*
    (channel
     (name 'systemic)
     (url "https://github.com/ReilySiegel/systemic")
@@ -24,14 +24,7 @@
       "89ff5b4374e472194eff08f2a69153d5cde6784e"
       (openpgp-fingerprint
        "0FA2 FE4C 164F 60C6 7F6B  EA7E 508A 5AD0 A50F 88AF"))))
-   (channel
-    (name 'guix)
-    (url "https://github.com/ReilySiegel/guix.git")
-    (introduction
-     (make-channel-introduction
-      "fe46280dbf2f518b9c96059df2f4d0e60b6fb3c7q"
-      (openpgp-fingerprint
-       "0FA2 FE4C 164F 60C6 7F6B  EA7E 508A 5AD0 A50F 88AF"))))))
+   %default-channels))
 
 
 (define (channel-skel skeleton)
