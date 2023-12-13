c1c221f2ea6bbee0a103c8feda3285554bccc337
Reily Siegel <mail@reilysiegel.com>
2023-12-13 09:48:38 -0500

    home: shell: Improve tramp-eshell support.

--- systemic/home/emacs/shell.scm
+++ systemic/home/emacs/shell.scm
@@ -18,6 +18,13 @@
                remote-file-name-inhibit-cache nil
                vc-ignore-dir-regexp
                (format "%s\\|%s" vc-ignore-dir-regexp tramp-file-name-regexp))))
+    ("tramp"
+     (with-eval-after-load 'tramp
+       (setopt tramp-show-ad-hoc-proxies t
+               tramp-ssh-controlmaster-options
+               (concat
+                "-o ControlPath=/tmp/ssh-ControlPath-%%r@%%h:%%p "
+                "-o ControlMaster=auto -o ControlPersist=yes"))))
     (emacs-eat
      (setopt eshell-visual-commands nil
              ;; Better remote compatibility.
