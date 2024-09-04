347c00f913afe158a9b2bc1d1756b5a9932eb3b1
Reily Siegel <mail@reilysiegel.com>
2024-09-04 15:15:35 -0400

    Use gcc as $CC.

--- systemic/machines/base.scm
+++ systemic/machines/base.scm
@@ -82,6 +82,7 @@
          ("GUILE_LOAD_COMPILED_PATH" .
           "$XDG_CONFIG_HOME/guix/current/lib/guile/3.0/site-ccache:$GUILE_LOAD_COMPILED_PATH")
          ("GUILE_AUTO_COMPILE" . "0")
+         ("CC" . "gcc")
          ("PATH" . "$HOME/.local/bin:$HOME/.local/share/flatpak/exports/bin/:$PATH")
          ;; HACK: https://issues.guix.gnu.org/52672
          ("QTWEBENGINE_CHROMIUM_FLAGS" . "--disable-seccomp-filter-sandbox")))
