5e456ef900da4ea03b712b390d4a3d34b99a746c
Reily Siegel <mail@reilysiegel.com>
2025-11-10 16:24:10 -0500

    tmp

--- systemic/home/desktop.scm
+++ systemic/home/desktop.scm
@@ -1,11 +1,15 @@
 (define-module (systemic home desktop)
   #:use-module (gnu home services)
   #:use-module (gnu home services desktop)
-  #:use-module (gnu home services sway)
+  #:use-module (gnu home services niri)
   #:use-module (gnu home services shepherd)
   #:use-module (gnu home services sound)
   #:use-module (gnu packages emacs)
+  #:use-module (gnu packages fonts)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages gnome)
   #:use-module (gnu packages linux)
+  #:use-module (gnu packages music)
   #:use-module (gnu packages package-management)
   #:use-module (gnu packages pulseaudio)
   #:use-module (gnu packages wm)
@@ -29,7 +33,7 @@
            (lambda (_)
              (list (shepherd-service
                     (provision '(mako))
-                    (requirement '(dbus sway))
+                    (requirement '(dbus niri))
                     (start #~(make-forkexec-constructor
                               (list (string-append #$mako "/bin/mako")
                                     "--ignore-timeout" "1")))
@@ -38,48 +42,17 @@
 
 (define services
   (list 
-   (service home-sway-service-type
-            (sway-configuration
-             (inputs
-              (list
-               (sway-input (identifier "type:keyboard")
-                           (layout
-                            (keyboard-layout "us" #:options '("ctrl:nocaps"))))
-               (sway-input (identifier "type:touchpad")
-                           (extra-content (list "natural_scroll enabled"
-                                                "accel_profile flat")))))
-             (outputs
-              (list (sway-output
-                     (identifier '*)
-                     (background (file-append sway "/share/backgrounds/sway/Sway_Wallpaper_Blue_1920x1080.png"))
-                     (extra-content (list "scale 1.5")))))
-             (bar
-              (sway-bar
-               (position 'top)
-               (status-command (file-append i3status "/bin/i3status"))
-               (colors
-                (sway-color
-                 (background "#2e3440")
-                 (statusline "#eceff4")))
-               (extra-content (list "status_padding 8"
-                                    "status_edge_padding 8"
-                                    "separator_symbol \" | \""))))
-             (extra-content
-              (list "default_border none"))))
+   (service home-niri-service-type)
    (service home-dbus-service-type)
    (service home-pipewire-service-type)
    (service mako-service-type)
-   (simple-service 'sway-launch
-                   home-shepherd-service-type
-                   (list
-                    (shepherd-service
-                     (provision '(sway))
-                     (requirement '(dbus))
-                     (start #~(make-forkexec-constructor
-                               (list (string-append #$sway "/bin/sway"))))
-                     (stop #~ (make-kill-destructor)))))
    (simple-service
     'flatpak-profile home-profile-service-type (list flatpak))
+   (simple-service
+    'desktop-profile home-profile-service-type
+    (list swayidle swaylock fuzzel waybar wireplumber brightnessctl
+          playerctl font-awesome
+          xdg-desktop-portal-gtk xdg-desktop-portal-gnome))
    (simple-service
     'flatpak-env home-environment-variables-service-type
     '(("XDG_DATA_DIRS" .
