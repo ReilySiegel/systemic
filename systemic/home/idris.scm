5d76924d7b7a4d79f54b9f13976f0cc4f634cc8c
Reily Siegel <mail@reilysiegel.com>
2025-11-10 13:17:43 -0500

    home: idris: Pack now uses XDG_DIR.

--- systemic/home/idris.scm
+++ systemic/home/idris.scm
@@ -13,15 +13,15 @@
     'idris-emacs home-emacs-service-type
     (emacs-configuration-extension
      (emacs-idris-mode
-      (with-eval-after-load 'flycheck
-        (require 'flycheck-idris)
-        (add-hook 'idris-mode-hook #'flycheck-mode))
       (with-eval-after-load 'aggressive-indent
         (add-to-list 'aggressive-indent-excluded-modes 'idris-mode))
       (setopt idris-interpreter-path "idris2"
-              idris-stay-in-current-window-on-compiler-error t))))
-   (simple-service
-    'idris-env
-    home-environment-variables-service-type
-    `(("PATH" . "$HOME/.pack/bin:$PATH")))))
+              idris-repl-show-repl-on-startup nil
+              idris-hole-show-on-load nil
+              idris-show-help-text nil
+              idris-completion-via-compiler nil
+              idris-warnings-printing (list 'warnings-repl)
+              idris-stay-in-current-window-on-compiler-error t)
+      (remove-hook 'idris-mode-hook 'turn-on-eldoc-mode)
+      (add-hook 'idris-mode-hook (lambda () (eldoc-mode -1))))))))
 
