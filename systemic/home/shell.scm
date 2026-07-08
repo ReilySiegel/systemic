548e56efb0731ab8c3d96b4cb66e3be18a1c7bbc
Reily Siegel <mail@reilysiegel.com>
2026-07-08 16:21:40 -0400

    home: shell: Switch from eat to ghostel.

--- systemic/home/shell.scm
+++ systemic/home/shell.scm
@@ -4,37 +4,29 @@
   #:use-module (gnu services)
   #:use-module (guix gexp)
   #:use-module (rde home services emacs)
+  ;; TODO: Update use rde ghostel when updated
+  #:use-module (systemic packages ghostty)
   #:use-module (systemic home emacs-utils)
   #:export (systemic-shell-service-type))
-
-(define eat-bash-integration
-  (plain-file
-   "source-eat-integration"
-   "
-[ \"$EAT_SHELL_INTEGRATION_DIR\" ] && source \"$EAT_SHELL_INTEGRATION_DIR/bash\"
-[ -n \"$__eat_integration_enabled\" ] && TERM=xterm-256color"))
-
-(define (bash-extension _)
-  (home-bash-extension
-    (bashrc (list eat-bash-integration))))
 
 (define (emacs-extension _)
   (emacs-configuration-extension
    ("tramp"
     (with-eval-after-load 'tramp
       (setopt tramp-show-ad-hoc-proxies t)))
-   (emacs-eat
-    (keymap-global-set "C-c s" #'eat)
-    (setopt eat-enable-shell-prompt-annotation nil
-            eat-kill-buffer-on-exit t)
-    (add-hook 'eat-mode-hook
-              (lambda () (setq-local scroll-conservatively 101)))
+   (emacs-ghostel
+    (keymap-global-set
+     "C-c s"
+     (lambda ()
+       (interactive)
+       (let* ((default-directory "~"))
+         (ghostel))))
 
     (with-eval-after-load 'project
       (keymap-unset project-prefix-map "e")
       (assq-delete-all 'project-eshell project-switch-commands)
-      (keymap-set project-prefix-map "s" #'eat-project)
-      (add-to-list 'project-switch-commands '(eat-project "Shell") t)))
+      (keymap-set project-prefix-map "s" #'ghostel-project)
+      (add-to-list 'project-switch-commands '(ghostel-project "Shell") t)))
    (emacs-buffer-env
     (setopt buffer-env-script-name "guix.scm")
     (add-hook 'hack-local-variables-hook #'buffer-env-update)
@@ -47,10 +39,9 @@
 
 (define systemic-shell-service-type
   (service-type
-    (name 'systemic-shell)
-    (description "Shell configuration")
-    (default-value #f)
-    (extensions
-     (list
-      (service-extension home-bash-service-type bash-extension)
-      (service-extension home-emacs-service-type emacs-extension)))))
+   (name 'systemic-shell)
+   (description "Shell configuration")
+   (default-value #f)
+   (extensions
+    (list
+     (service-extension home-emacs-service-type emacs-extension)))))
